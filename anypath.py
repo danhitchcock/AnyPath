2d28efd8866f866797eaec56483bd0d9c54385a8
daniel.hitchcock <daniel.hitchcock@centralsquare.com>
2020-01-16 18:12:39 -0500

    -initial commit

---
+++ anypath.py
@@ -0,0 +1,100 @@
+import re
+import copy
+
+
+class AnyPath:
+    def __init__(self, path=None):
+
+        self.sep = "/"
+        self.prefix = "/"
+        self.is_root = False
+        self.flavor = "posix"
+
+        # first check if it's a posix root path
+        if path.startswith("/"):
+            self.prefix = "/"
+            self.is_root = True
+
+        # now check if it's a url-like object
+        elif path.find("://") > -1:
+            self.prefix = re.search(".*://", path)[0]
+            self.is_root = True
+            self.sep = "/"
+            self.flavor = "url"
+
+        # if there is a backslash, it's a windows like object
+        elif path.find("\\") > -1:
+            re_result = re.search(".*:\\\\", path)
+            if re_result:
+                self.prefix = re_result[0]
+                self.is_root = True
+            self.flavor = "windows"
+            self.sep = "\\"
+
+        pre_len = len(self.prefix) if self.is_root else 0
+        self.components = [p for p in path[pre_len:].split(self.sep) if p]
+        self.is_dir = False
+        if path.endswith(self.sep):
+            self.is_dir = True
+
+    def __str__(self):
+        pathstr = self.sep.join(self.components)
+        if self.is_root:
+            pathstr = self.prefix + pathstr
+        if self.is_dir:
+            pathstr += self.sep
+        return pathstr
+
+    def __add__(self, other):
+        a = copy.deepcopy(self)
+        a.components += other.components
+        a.is_dir = other.is_dir
+        return a
+
+    def __radd__(self, other):
+        if other is not 0:
+            return self + other
+        return self
+
+    def __truediv__(self, other):
+        return self + other
+
+    @property
+    def suffix(self):
+        if self.is_dir:
+            raise ValueError("This object appears to be a directory.")
+        if self.components[-1].find(".") > -1:
+            return self.components[-1].split(".")[-1]
+        return None
+
+
+a = AnyPath("/home/files.jpg")
+print(str(a), a.suffix, a.is_root)
+
+a = AnyPath("http://home//files.jpg")
+print(str(a), a.suffix, a.is_root)
+
+a = AnyPath("s3://home/files.jpg")
+print(str(a), a.suffix, a.is_root)
+
+a = AnyPath("home/files.jpg")
+print(str(a), a.suffix, a.is_root)
+
+a = AnyPath(r"C:\home\files.jpg")
+print(str(a), a.suffix, a.is_root)
+
+a = AnyPath(r"home\files.jpg")
+print(str(a), a.suffix, a.is_root)
+
+a = AnyPath(r"/home/files")
+print(str(a), a.suffix, a.is_root)
+
+a = AnyPath(r"/home/files/")
+print(str(a))
+
+b = AnyPath(r"some/more/folders/pic.jpg")
+print(a + b)
+print(a / b)
+
+c = sum([a, b, a, b])
+print(c)
